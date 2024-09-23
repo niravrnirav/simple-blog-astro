8eab9e31644f1c98e65914a017e2538af706f422
Nirav Patel <npatel@obsglobal.com>
2024-09-23 18:39:05 -0500

    updated/removed logos/socials

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,7 +1,7 @@
 export const settings = {
   site: 'https://simple-blog.littlesticks.dev',
-  name: 'Simple Blog',
+  name: 'Top Products',
   title: 'Site title',
   description: 'Site description',
-  showLittleSticksPlug: true,
+  showLittleSticksPlug: false,
 }
